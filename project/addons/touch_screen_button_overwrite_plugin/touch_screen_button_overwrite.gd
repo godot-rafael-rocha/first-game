692e29e7911ccd42fe1c60237799c4f5fe81ca41
Rafael Rudá Rocha Cordeiro Guedes <rrocha.rafael@gmail.com>
2024-11-12 22:43:39 -0300

    Update touch_screen_button_overwrite.gd

--- project/addons/touch_screen_button_overwrite_plugin/touch_screen_button_overwrite.gd
+++ project/addons/touch_screen_button_overwrite_plugin/touch_screen_button_overwrite.gd
@@ -3,6 +3,7 @@ class_name TouchScreenButtonOverwrite
 
 var _debug = false
 var pointerDictionary = {}
+var lastInputEvent: InputEvent
 enum pointerDictionaryPropertiesName {
 	pointerPosition,
 	fingerIndex
@@ -10,17 +11,21 @@ enum pointerDictionaryPropertiesName {
 signal pointer_position_changed(event: InputEvent)
 
 func _ready() -> void:
+	self.pressed.connect(func (): _update_pointer_position(true))
+	self.released.connect(func (): _update_pointer_position(false))
 	if _debug:
 		self.pointer_position_changed.connect(func (event): print("pointer_position_changed: ", event))
 		self.pressed.connect(func (): print("pressed"))
 		self.released.connect(func (): print("released"))
 
-func _update_pointer_position(event: InputEvent):
+func _update_pointer_position(is_pressed_arg: bool):
+	var event = lastInputEvent
 	var lastPointerPosition = pointerDictionary.get(pointerDictionaryPropertiesName.pointerPosition);
 	var lastfingerIndex = pointerDictionary.get(pointerDictionaryPropertiesName.fingerIndex);
 	var newpointerPosition: Vector2
 	var newfingerIndex: int
-	if is_pressed():
+	
+	if is_pressed_arg:
 		if event is InputEventMouseMotion || event is InputEventMouseButton:
 			newpointerPosition = event.position
 		elif !lastPointerPosition && event is InputEventScreenTouch:
@@ -34,13 +39,14 @@ func _update_pointer_position(event: InputEvent):
 	else:
 		pointerDictionary.clear()
 		
-	if is_pressed() && newpointerPosition && newpointerPosition != lastPointerPosition:
+	if is_pressed_arg && newpointerPosition && newpointerPosition != lastPointerPosition:
 		pointerDictionary[pointerDictionaryPropertiesName.pointerPosition] = newpointerPosition
 		pointerDictionary[pointerDictionaryPropertiesName.fingerIndex] = newfingerIndex
 		emit_signal("pointer_position_changed", event)
 
 func _input(event: InputEvent) -> void:
-	_update_pointer_position(event)
+	lastInputEvent = event
+	_update_pointer_position(is_pressed())
 
 func get_pointer_position() -> Vector2:
 	var result = pointerDictionary.get(pointerDictionaryPropertiesName.pointerPosition)
